6a9302072d81333349e625dc85ea9e86355d237b
rooot <rooot@rooots-MacBook-Air.local>
2026-01-14 01:05:05 +0100

    minishell.h normed

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -3,124 +3,133 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atbicer <atbicer@student.42belgium.be>     +#+  +:+       +#+        */
+/*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 12:31:13 by aschweit          #+#    #+#             */
-/*   Updated: 2026/01/14 00:53:47 by atbicer          ###   ########.fr       */
+/*   Updated: 2026/01/14 01:03:36 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <termios.h>
-#include "libft.h"
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <termios.h>
+# include <unistd.h>
 
+/* token types
+	Pipe = "|"
+	Redir_in = "<"
+	Redir_app = "<<"
+	Redir_out = ">"
+	Redir_append = ">>"
+	Var = "$"
+*/
 typedef enum token_type
 {
-    type_Word,
-    type_Pipe,       // "|"
-    type_Redir_in,     // "<"
-    type_Redir_app,      // "<<"
-    type_Redir_out,    // ">"
-    type_Redir_append,     // ">>"
-    type_Var,   // "$"
-    type_Var_exit,
-    type_Var_pid
-
-}   t_token_type;
+	type_Word,
+	type_Pipe,
+	type_Redir_in,
+	type_Redir_app,
+	type_Redir_out,
+	type_Redir_append,
+	type_Var,
+	type_Var_exit,
+	type_Var_pid
+}						t_token_type;
 
 typedef struct s_token
 {
-    t_token_type    type;
-    char            *value;
-    int             in_single_quotes;
-    int             in_double_quotes;
-    struct s_token  *next;
-}   t_token;
+	t_token_type		type;
+	char				*value;
+	int					in_single_quotes;
+	int					in_double_quotes;
+	struct s_token		*next;
+}						t_token;
 
 typedef struct s_quote
 {
-    int in_single; 
-    int in_double;  
-}   t_quote;
+	int					in_single;
+	int					in_double;
+}						t_quote;
 
 typedef struct s_redir
 {
-    int             type;
-    char            *file;
-    struct s_redir  *next;
-}   t_redir;
+	int					type;
+	char				*file;
+	struct s_redir		*next;
+}						t_redir;
 
 typedef struct s_cmd
 {
-    char            **argv;
-    t_redir         *redirs;
-    struct s_cmd    *next;
-}   t_cmd;
+	char				**argv;
+	t_redir				*redirs;
+	struct s_cmd		*next;
+}						t_cmd;
 
-extern struct termios g_original_term;
+extern struct termios	g_original_term;
 
-t_token *tokenize_with_quotes(char *input);
-t_token *extract_word_with_quotes(char *str, int *i);
-t_token *new_token(t_token_type type, char *value);
-char    *extract_var_name(char *str, int *i);
-int is_token_char(char c);
-int is_space(char c);
-int is_var_char(char c);
-void    add_token(t_token **head, t_token *new);
-void print_tokens(t_token *tokens);
-void free_tokens(t_token *tokens);
-void free_commands(t_cmd *cmds);
-t_cmd *parse(t_token *tokens);
-char    **build_argv(t_token **tokens, t_cmd *cmd);
-t_cmd *new_cmd(void);
-void print_commands(t_cmd *cmds);
-int is_redir(t_token_type type);
-t_redir *new_redir(int type, char *file);
-void add_redir(t_redir **head, t_redir *new);
-t_redir *parse_redir(t_token **tokens);
-int check_syntax(t_token *tokens);
-char *remove_quotes(char *str);
+t_token					*tokenize_with_quotes(char *input);
+t_token					*extract_word_with_quotes(char *str, int *i);
+t_token					*new_token(t_token_type type, char *value);
+char					*extract_var_name(char *str, int *i);
+int						is_token_char(char c);
+int						is_space(char c);
+int						is_var_char(char c);
+void					add_token(t_token **head, t_token *new);
+void					print_tokens(t_token *tokens);
+void					free_tokens(t_token *tokens);
+void					free_commands(t_cmd *cmds);
+t_cmd					*parse(t_token *tokens);
+char					**build_argv(t_token **tokens, t_cmd *cmd);
+t_cmd					*new_cmd(void);
+void					print_commands(t_cmd *cmds);
+int						is_redir(t_token_type type);
+t_redir					*new_redir(int type, char *file);
+void					add_redir(t_redir **head, t_redir *new);
+t_redir					*parse_redir(t_token **tokens);
+int						check_syntax(t_token *tokens);
+char					*remove_quotes(char *str);
 
 /* exec */
-int execute_cmds(t_cmd *cmds, char **envp);
+int						execute_cmds(t_cmd *cmds, char **envp);
 
 /* builtin functions */
-int builtin_pwd(char **argv);
-int	builtin_echo(char **argv);
-int	builtin_cd(char **argv);
-int	builtin_env(char **argv, char **envp);
-int	builtin_exit(char **argv);
-int	builtin_export(char **argv, char **envp);
-int	builtin_unset(char **argv);
-
+int						builtin_pwd(char **argv);
+int						builtin_echo(char **argv);
+int						builtin_cd(char **argv);
+int						builtin_env(char **argv, char **envp);
+int						builtin_exit(char **argv);
+int						builtin_export(char **argv, char **envp);
+int						builtin_unset(char **argv);
 
 /* norme merge new prototypes */
-int	check_pipe_syntax(t_token *current);
-int	check_redir_syntax(t_token *current);
-void	free_redirs(t_redir *redir);
-void	free_argv(char **argv);
-void	add_word(t_token *curr, char **argv, int *i);
-void	add_cmd(t_cmd **head, t_cmd *new);
-int	count_args(t_token *tokens);
-void	process_char_in_quotes(char *str, int *i, char *res, int *j);
-int	handle_escape_in_dquotes(char *str, int *i, char *res, int *j);
-char	*extract_normal_var(char *str, int start, int end);
-void	add_pipe(t_token **tokens, int *i);
-void	add_redir_out(t_token **tokens, char *input, int *i);
-void	add_redir_in(t_token **tokens, char *input, int *i);
-t_token	*init_token(void);
-char	*copy_word(char *str, int start, int end);
-void	process_word(char *str, int *i, t_quote *state);
-char	*create_special_var(char c);
-t_token	*create_var_token(t_token_type type, char c);
-t_token	*handle_var(char *str, int *i);
-t_cmd	*parse(t_token *tokens);
-void	update_quote_state(char c, t_quote *state, t_token *token);
-int	process_redir(t_token **current, t_cmd *cmd);
-t_redir	*new_redir(int type, char *file);
+int						check_pipe_syntax(t_token *current);
+int						check_redir_syntax(t_token *current);
+void					free_redirs(t_redir *redir);
+void					free_argv(char **argv);
+void					add_word(t_token *curr, char **argv, int *i);
+void					add_cmd(t_cmd **head, t_cmd *new);
+int						count_args(t_token *tokens);
+void					process_char_in_quotes(char *str, int *i, char *res,
+							int *j);
+int						handle_escape_in_dquotes(char *str, int *i, char *res,
+							int *j);
+char					*extract_normal_var(char *str, int start, int end);
+void					add_pipe(t_token **tokens, int *i);
+void					add_redir_out(t_token **tokens, char *input, int *i);
+void					add_redir_in(t_token **tokens, char *input, int *i);
+t_token					*init_token(void);
+char					*copy_word(char *str, int start, int end);
+void					process_word(char *str, int *i, t_quote *state);
+char					*create_special_var(char c);
+t_token					*create_var_token(t_token_type type, char c);
+t_token					*handle_var(char *str, int *i);
+t_cmd					*parse(t_token *tokens);
+void					update_quote_state(char c, t_quote *state,
+							t_token *token);
+int						process_redir(t_token **current, t_cmd *cmd);
+t_redir					*new_redir(int type, char *file);
 #endif
